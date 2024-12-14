55f1a0287148c053176292d8902eaf68188df189
LinhDancute <dannguyen11003@gmail.com>
2024-12-14 14:57:53 +0700

    First Commit

---
+++ PRDB_Sqlite/BLL/ConditionModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace PRDB_Sqlite.BLL
+{
+    public class ConditionModel
+    {
+        public List<StrategyModel> StrategyModels { get; set; }
+        public List<string> OperatorStrategy { get; set; }
+        public double? MaxProb { get; set; }
+        public double? MinProb { get; set; }
+    }
+
+    public class StrategyModel
+    {
+        public string AttributeName { get; set; }
+        public string AttributeValue { get; set; }
+        public string OperatorStrOfTriple { get; set; }
+    }
+}
